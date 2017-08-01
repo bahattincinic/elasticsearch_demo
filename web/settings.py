37a55f82d4895a0b2199694fd5eb30e70fba7744
Bahattin Cinic <bahattin@adphorus.com>
2017-08-01 11:56:10 +0300

    settings updated

--- web/settings.py
+++ web/settings.py
@@ -1,7 +1,10 @@
+import os
+
+
 class Config(object):
     DEBUG = False
-    FOURSQUARE_CLIENT_ID = '<Your Client Id>'
-    FOURSQUARE_CLIENT_SECRET = '<Your Client Secret>'
+    FOURSQUARE_CLIENT_ID = 'HD2ROBTRKGWOSTUS4MCEMTNFT5CO4MCXR0GRXA0STXAQJRU5'
+    FOURSQUARE_CLIENT_SECRET = 'OCXEGH4HK4XDPM0SAKSXQJBWJYRNMF5KF55KOH4KUPPP224J'
     ELASTICSEARCH_URL = '127.0.0.1:9200'
     ELASTICSEARCH_NUMBER_OF_SHARDS = 5
     PER_PAGE = 25
@@ -13,14 +16,14 @@ class Config(object):
             'properties': {
                 'title': {
                     'type': 'string',
-                    'index' : 'not_analyzed'
+                    'index': 'not_analyzed'
                 },
                 'point': {
                     'type': 'geo_point'
                 },
                 'category': {
                     'type': 'string',
-                    'index' : 'not_analyzed'
+                    'index': 'not_analyzed'
                 }
             }
         }
@@ -29,3 +32,8 @@ class Config(object):
 
 class DevelopmentConfig(Config):
     DEBUG = True
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+    ELASTICSEARCH_URL = os.getenv('ELASTICSEARCH_URL', '127.0.0.1:9200')
